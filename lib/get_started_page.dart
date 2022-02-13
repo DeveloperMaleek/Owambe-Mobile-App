a791db7a23a2be1f372872915947ccc7c5cf975e
Malik Kolade <kolademalikademola@gmail.com>
2022-02-13 05:59:40 +0000

    refactored my app. App is at clean state

---
+++ lib/get_started_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class GetStartedPage extends StatefulWidget {
+  const GetStartedPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<GetStartedPage> createState() => _GetStartedPageState();
+}
+
+class _GetStartedPageState extends State<GetStartedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
